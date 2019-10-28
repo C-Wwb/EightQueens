b52e776328a59f02c899dfdaa9a5515cebb9db26
C-Wwb <1729220020@qq.com>
2019-10-28 20:02:30 +0800

    second

---
+++ cmake-build-debug/CMakeFiles/EightQueen.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EightQueen.dir/main.cpp.obj"
+  "EightQueen.exe"
+  "EightQueen.exe.manifest"
+  "EightQueen.pdb"
+  "libEightQueen.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EightQueen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
